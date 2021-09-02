856c6120269b946477f639e746204c98cea1e33e
yoon92kr <yoon92_kr@naver.com>
2021-09-02 21:53:55 +0900

    2021.09.02 List 연산자 ▶ list 연산자는 연결 연산자, 곱셈을 제공한다 ▶ len 메소드는 list 의 총 길이를 반환해준다

---
+++ Code025.py
@@ -0,0 +1,15 @@
+# 리스트 연산자
+list_a = [1, 2, 3]
+list_b = [4, 5, 6]
+
+print("list_a : ", list_a)
+print("list_b : ", list_b)
+print()
+
+#기본 연산자
+print("list_a * 3 :",list_a*3)
+print("list_a + list_b :", list_a + list_b)
+print()
+
+#함수
+print('len(list_a) :',len(list_a))
