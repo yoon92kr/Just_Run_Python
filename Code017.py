c8a158eecd0b35bc1e666893486ff570890fc4c6
yoon92kr <yoon92_kr@naver.com>
2021-09-02 21:45:07 +0900

    2021.09.02 import ▶ 외부모듈을 대입하기 위한 예약어이다

---
+++ Code017.py
@@ -0,0 +1,11 @@
+# 외부모듈 import
+import datetime
+
+now = datetime.datetime.now()
+
+print(now.year, "년")
+print(now.month, "월")
+print(now.day, "일")
+print(now.hour, "시")
+print(now.minute, "분")
+print(now.second, "초")
