44472c69b9c63d0ca9e5e9f04ef4a2afb2dca4f3
yoon92kr <yoon92_kr@naver.com>
2021-09-02 21:54:58 +0900

    2021.09.02 List 관련 메소드 ▶ append(A) : A를 List 마지막 index 에 추가 ▶ insert(A, B) : A index에 B의 값을 삽입 ▶ extend(A) : 다량의 값(list)를 마지막 index 에 추가

---
+++ Code026.py
@@ -0,0 +1,15 @@
+# 리트스 관련 메소드
+list = [1,2,3]
+print(list)
+
+# 매개변수 값을 list 마지막 index에 추가
+list.append(4)
+print(list)
+
+# 매개변수 A index 에 B 값을 추가
+list.insert(0, 5)
+print(list)
+
+# 다량의 값을 list 마지막 index에 추가
+list.extend([6,7,8])
+print(list)
