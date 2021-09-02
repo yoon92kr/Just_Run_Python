5a730f092fe23219d363204e5a6fb431388d6887
yoon92kr <yoon92_kr@naver.com>
2021-09-02 21:38:17 +0900

    2021.09.02 숫자연산 프로그램 1 ▶ 원시적인 코딩 방법

---
+++ Code012.py
@@ -0,0 +1,12 @@
+#숫자 연산 프로그램
+
+intput_a = float(input("첫 번째 숫자 > "))
+intput_b = float(input("두 번째 숫자 > "))
+print()
+
+print("덧셈 결과 : ", intput_a+intput_b)
+print("뺄셈 결과 : ", intput_a-intput_b)
+print("곱셈 결과 : ", intput_a*intput_b)
+print("나눗셈 결과 : ", intput_a/intput_b)
+print("몫 결과 : ", intput_a//intput_b)
+print("나머지 결과 : ", intput_a%intput_b)
