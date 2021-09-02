22499a07da59a21045d5f3021ce7707b40ea563a
yoon92kr <yoon92_kr@naver.com>
2021-09-02 21:42:48 +0900

    2021.09.02 논리연산자 ▶ 논리연산자는 논리부정(not) 논리곱(and) 논리합(or)로 이루어진다.

---
+++ Code015.py
@@ -0,0 +1,22 @@
+# 논리연산자
+
+# 논리 부정 _ not
+print(not True)  # Ture 을 부정
+print(not False) 
+
+x = 10
+is_under_20 = x < 20
+print("is_under_20 : ", is_under_20)
+print("not is_under_20 : ", not is_under_20)
+
+# 논리 곱 _ and _ 둘 중 하나라도 거짓이면 거짓
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# 논리 합 _ or _ 둘 중 하나라도 참이면 참
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
