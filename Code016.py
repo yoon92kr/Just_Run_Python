ead3f15fb153defd6774d8ba70fb345bb37fad4b
yoon92kr <yoon92_kr@naver.com>
2021-09-02 21:44:13 +0900

    2021.09.02 if 조건문 ▶ if 조건문 선언 형식 if 조건: (들여쓰기)처리문 ▶ python의 if 조건문은 들여쓰기를 통해 종속 구분을 하기때문에 유의해야한다

---
+++ Code016.py
@@ -0,0 +1,16 @@
+# if 조건문
+# if 조건: 참일경우 처리문
+number = input("정수를 입력하세요")
+number = int(number)
+
+if number > 0:
+     print("입력값은 양수입니다.")
+
+if number == 0:
+     print("입력값은 0 입니다.")
+
+if number < 0:
+     print("입력값은 음수입니다.")
+
+    
+    
