551bb77d674d1216bad34e1667d550482affaddd
yoon92kr <yoon92_kr@naver.com>
2021-09-04 20:23:15 +0900

    2021.09.03 list 메소드2

---
+++ Code029.py
@@ -0,0 +1,13 @@
+# list 메소드
+list = [0,1,2,3,4,5,0,1,2,3,4,5]
+print("list : ", list)
+
+# remove뒤의 매개변수 값과 같은 값을 제거, 단 처음 만나는 값만 제거
+list.remove(0)
+print("list.remove(0) :",list)
+print()
+
+# 리스트 내 모든 값 제거
+list.clear()
+print("list.claer() : ",list)
+
