9854e6ac4d18cfe0cfcfec126dea38869fa3aada
yoon92kr <yoon92_kr@naver.com>
2021-09-05 15:33:21 +0900

    2021.09.03 재귀함수2

---
+++ Code051.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    if n ==1:
+        return 1
+    elif n ==2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+print("fibonacci(1) :", fibonacci(1))
+print("fibonacci(1) :", fibonacci(2))
+print("fibonacci(1) :", fibonacci(3))
+print("fibonacci(1) :", fibonacci(4))
+print("fibonacci(1) :", fibonacci(5))
+print("fibonacci(1) :", fibonacci(6))
+print("fibonacci(1) :", fibonacci(7))
+
+        
