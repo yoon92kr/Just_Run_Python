9ff0b8f1095db35034ff9b13b46c40485984ba56
yoon92kr <yoon92_kr@naver.com>
2021-09-06 20:30:03 +0900

    2021.09.06 sys 모듈 ▶ 이하 생략

---
+++ Code074.py
@@ -0,0 +1,14 @@
+# sys 모듈_시스템 관련 정보를 지원해주는 모듈
+
+import sys
+
+print(sys.argv)
+print("-----")
+
+print("sys.getwindowserversion() : ", sys.getwindowsversion())
+print("-----")
+print("sys.copyright : ", sys.copyright)
+print("-----")
+print("sys.version : ", sys.version)
+
+sys.exit()
