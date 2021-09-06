b99a4f9f4594e601f8a8bc2ea38a8ded92f303e5
yoon92kr <yoon92_kr@naver.com>
2021-09-06 20:29:33 +0900

    2021.09.06 import as ▶ 선언 형식 import 모듈명 as 변수명 ▶해당 변수에 호출한 모듈을 대입한것으로, 변수명.메소드를 통해 호출이 가능해진다 ▶ 보통 모듈명이 중복되거나, 필요에의해 사용된다

---
+++ Code072.py
@@ -0,0 +1,11 @@
+# import 모듈명 as 변수명
+# 변수명에 모듈을 대입 후, 변수명으로 모듈을 사용
+
+import math as m
+
+print("m.sin(1) : ", m.sin(1))
+print("m.cos(1) : ", m.cos(1))
+print("m.tan(1) : ", m.tan(1))
+
+print("m.floor(2.5) : ", m.floor(2.5))    #내림
+print("m.ceil(2.5) : ", m.ceil(2.5))      #올림
