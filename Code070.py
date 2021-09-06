c7764e3bda30c93b824328ac0602762f1b4f0646
yoon92kr <yoon92_kr@naver.com>
2021-09-06 20:27:16 +0900

    2021.09.06 import ▶ 타 모듈을 사용할 수 있게 해주는 키워드이다 ▶ import 모듈명 으로 호출된 모듈을 사용할 경우 모듈명.메소드 형식을 취해야한다

---
+++ Code070.py
@@ -0,0 +1,12 @@
+# 모듈 import
+# python이 제공하는 기본모듈 사용법
+# 불러온 모듈.메소드 형식으로 호출할 수 있다.
+
+import math
+
+print("math.sin(1) : ", math.sin(1))
+print("math.cos(1) : ", math.cos(1))
+print("math.tan(1) : ", math.tan(1))
+
+print("math.floor(2.5) : ", math.floor(2.5))    #내림
+print("math.ceil(2.5) : ", math.ceil(2.5))      #올림
