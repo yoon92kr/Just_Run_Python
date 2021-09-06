1ff01f5b8ab0b0a0e05e8effedcb5c354e0099a3
yoon92kr <yoon92_kr@naver.com>
2021-09-06 20:28:16 +0900

    2021.09.06 from ▶ 모듈 내 특정 메소드만 사용해야 할 경우 사용 ▶ 선언 형식 from 모듈명 import 메소드명, 메소드명, .... ▶ from을 통해 호출한 특정 메소드들은, 모듈명 없이 바로 직접호출이 가능하다

---
+++ Code071.py
@@ -0,0 +1,13 @@
+# from import 모듈
+# python이 제공하는 기본모듈 사용법
+# 모듈 내 특정 메소드만 사용하고자 할때 선언한다
+# 별도의 모듈명이 아닌 메소드를 바로 사용 가능하다.
+
+from math import sin, cos, tan, floor, ceil
+
+print("sin(1) : ", sin(1))
+print("cos(1) : ", cos(1))
+print("tan(1) : ", tan(1))
+
+print("floor(2.5) : ", floor(2.5))    #내림
+print("ceil(2.5) : ", ceil(2.5))      #올림
