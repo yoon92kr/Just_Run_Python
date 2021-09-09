cf59dfae0c0e10f42abed5206c942fac8e359cb1
yoon92kr <yoon92_kr@naver.com>
2021-09-09 21:20:13 +0900

    2021.09.09 format %

---
+++ Code089.py
@@ -0,0 +1,15 @@
+# format %
+# %s 문자열 %d 정수 %f 실수 
+print('hello %s' % 'python')
+print('PI %d' % 3.14)
+print('PI %f' % 3.14)
+
+# 여러개일 경우
+a = "a"
+b = "b"
+cD = 1
+dD = 2
+eF = 1.1
+fF = 1.2
+
+print("a : %s, b : %s, c : %d, d : %d, e : %f, f : %f" %(a,b,cD,dD, eF, fF))
