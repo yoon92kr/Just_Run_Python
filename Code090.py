f2503828acbf3d121f3f67b482127f628ed87e59
yoon92kr <yoon92_kr@naver.com>
2021-09-09 21:20:35 +0900

    2021.09.09 f-string format

---
+++ Code090.py
@@ -0,0 +1,13 @@
+# f-string format
+# 선언 형식 f"{변수명, 자료값}"
+text = 'python'
+print(f"hello {text}")
+
+mount = 1
+while mount <= 12:
+    print(f"2021년 {mount}월")
+    mount += 1
+
+days = 1
+for day in days:
+    print(f"9월 {day}일")
