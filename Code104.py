01a5f3880a1d8fc8f7bd0e93852f68d35d83f600
yoon92kr <73813647+yoon92kr@users.noreply.github.com>
2021-11-01 09:17:53 +0900

    Code102.py 2021.11.01 pandas 3
    ▶ 메소드 활용 예제 2

---
+++ Code104.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+family_df = pd.read_csv('family.csv')
+family_add_df = pd.read_csv('family_new.csv')
+family_df.append(family_add_df, ignore_index= True)
+print(family_df)
+
+mean_age = family_df['나이'].mean().astype('int')
+family_df['나이'].fillna(mean_age, inplace=True)
+
+
+family_df.to_csv('가족.csv', index= False)
+family_df.to_json('가족.json')
